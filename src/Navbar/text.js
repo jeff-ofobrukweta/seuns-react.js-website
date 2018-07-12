ed98ab8e04d3308cc36f12b647ff72ffa31cef2a
jeff-ofobrukweta <oghenerukevwejeff@gmail.com>
2018-07-12 21:27:55 +0000

    checking animation css

--- src/Navbar/text.js
+++ src/Navbar/text.js
@@ -5,7 +5,7 @@ class Appanimate extends Component {
 
   render() {
     return (
-      <div className="Appanimate" style={{width:'40%',margin:'0px 190px'}}>
+      <div className="Appanimate" style={{width:'40%',height: '0px',margin:'0px 190px'}}>
         <p className="text-write" style={{color:'#e14800',fontWeight:1000,position:'relative'}}>INFORMATION</p>
       </div>
     );
